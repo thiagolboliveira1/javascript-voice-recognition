72893ec81e85f502c7786331065c2eb2ca17237f
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-06-27 18:29:29 -0300

    Jogo mario

--- js/functions.js
+++ js/functions.js
@@ -1,177 +1,102 @@
 var engine = {
-    "cores": ['green','purple','pink','red','yellow','orange','grey','black'],
+    "cores":['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'black', 'white', 'gray'],
     "hexadecimais":{
-      'green':'#02EF00',
-      'purple':'#790093',
-      'pink': '#F02A7E',
-      'red':'#E90808',
-      'yellow':'#E7D703',
-      'orange':'#F16529',
-      'grey':'#EBEBEB',
-      'black':'#141414',
+        'red':'#ff0000',
+        'blue':'#0000ff',
+        'green':'#008000',
+        'yellow':'#ffff00',
+        'purple':'#790093',
+        'orange':'#800080',
+        'pink':'#ffa500',
+        'black':'#000000',
+        'gray':'#808080',
     },
     "moedas":0
 }
 
 const audioMoeda = new Audio('audio/moeda.mp3');
-const audioErrou = new Audio('audio/errou.mp3');
+const audioErrou = new Audio('audio/errou.mp3')
 
-function sortearCor(){
-  var indexCorSorteada = Math.floor(Math.random() * engine.cores.length);
-  var legendaCorDaCaixa = document.getElementById('cor-na-caixa');
-  var nomeCorSorteada = engine.cores[indexCorSorteada];
+function sortearCor() {
+    var indexCorSorteada = corSorteada = Math.floor(Math.random() * engine.cores.length);
+    var legendaCorDaCaixa = document.getElementById('cor-na-caixa');
+    var nomeCorSorteada = engine.cores[indexCorSorteada];
 
-  legendaCorDaCaixa.innerText = nomeCorSorteada.toUpperCase();
-
-  return engine.hexadecimais[nomeCorSorteada];
+    legendaCorDaCaixa.innerText = nomeCorSorteada.toUpperCase();
+    return engine.hexadecimais[nomeCorSorteada]
 }
 
-
-function aplicarCorNaCaixa(nomeDaCor){
-  var caixaDasCores = document.getElementById('cor-atual');
-  
-  caixaDasCores.style.backgroundColor = nomeDaCor;
-  caixaDasCores.style.backgroundImage = "url('/img/caixa-fechada.png')";
-  caixaDasCores.style.backgroundSize = "100%";
-
+function aplicarCorNaCaixa(nomeDaCor) {
+    var caixaDasCores = document.getElementById('cor-atual');
+    caixaDasCores.style.backgroundColor = 'green';
+    caixaDasCores.style.backgroundImage = "url('/img/caixa-fechada.png')";
+    caixaDasCores.style.backgroundSize = "100%";
 }
-
 
 function atualizaPontuacao(valor){
-  var pontuacao = document.getElementById('pontuacao-atual');
+    var pontuacao = document.getElementById('pontuacao-atual');
 
-  engine.moedas += valor;
+    engine.moedas += valor;
 
-  if(valor < 0){
-    audioErrou.play();
-  }else{
-    audioMoeda.play();
-  }
+    if(valor < 0) {
+        audioErrou.play();
+    }
+    else{
+        audioMoeda.play();
+    }
 
-  pontuacao.innerText = engine.moedas;
+    pontuacao.innerText = engine.moedas;
 }
 
 aplicarCorNaCaixa(sortearCor())
+
 //API DE RECONHECIMENTO DE VOZ
 var btnGravador = document.getElementById("btn-responder");
-var transcricaoAudio = "";
+var trancricaoAudio = "";
 var respostaCorreta = "";
 
-if(window.SpeechRecognition || window.webkitSpeechRecognition){
-  var SpeechAPI = window.SpeechRecognition || window.webkitSpeechRecognition;
-  var gravador = new SpeechAPI();
 
-  gravador.continuos = false;
-  gravador.lang = "en-US";
+if(Window.SpeechRecognition || window.webkitSpeechRecognition) {
+    var SpeechAPI = Window.SpeechRecognition || window.webkitSpeechRecognition;
+    var gravador = new SpeechAPI();
+
+    gravador.continuos = false;
+    gravador.lang = "en-US";
 
 
-  gravador.onstart = function(){
-    btnGravador.innerText = "Estou Ouvindo";
-    btnGravador.style.backgroundColor = "white";
-    btnGravador.style.color = "black";
-  }
+    gravador.onstart = function(){
+        btnGravador.innerText = "Estou Ouvindo";
 
-  gravador.onend = function(){
-    btnGravador.innerText = "Responder";
-    btnGravador.style.backgroundColor = "transparent";
-    btnGravador.style.color = "white";
-  }
-
-  gravador.onresult = function(event){
-    transcricaoAudio = event.results[0][0].transcript.toUpperCase();
-    respostaCorreta = document.getElementById('cor-na-caixa').innerText.toUpperCase();
-
-    if(transcricaoAudio ===  respostaCorreta){
-      atualizaPontuacao(1);
-    }else{
-      atualizaPontuacao(-1);
+        btnGravador.style.backgroundColor = white;
+        btnGravador.style.color = black;
     }
 
-    aplicarCorNaCaixa(sortearCor());
+    gravador.onend = function(){
+        btnGravador.innerText = "Responder";
 
-  }
+        btnGravador.style.backgroundColor = transparent;
+        btnGravador.style.color = "white";
+    }
+
+    gravador.onresult = function(event){
+        transcricaoAudio = event.results[0][0].transcript.toUpperCase();
+        respostaCorreta = document.getElementById('cor-na-caixa').innerText.toUpperCase();
+
+        if(trancricaoAudio === respostaCorreta){
+        atualizarPontuacao(1);
+        }else{
+            atualizaPontuacao(-1);
+        }
+
+        aplicarCorNaCaixa(sortearCor());
+    }
 
 
-}else{
-  alert('não tem suporte');
+
+}else {
+    alert('Não tem suporte');
 }
 
-
-btnGravador.addEventListener('click', function(e){
-  gravador.start();
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btnGravador.addEventListener('click', function(e) {
+    gravador.start();
+});
